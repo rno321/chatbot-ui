648bc8653f08ece9817370e7df8933c3e5884401
rno321 <arno.burnuk@gmail.com>
2025-01-04 11:59:39 -0500

    added profile icon

--- components/sidebar/sidebar-logo.tsx
+++ components/sidebar/sidebar-logo.tsx
@@ -1,13 +1,17 @@
-import { FC } from "react"
-import { BrainCog } from "lucide-react"
+import { BrainCog, User2Icon } from "lucide-react"
 
 interface SidebarLogoProps {}
 
-export const SidebarLogo: FC<SidebarLogoProps> = ({}) => {
+export const SidebarLogo: FC<SidebarLogoProps> = () => {
   return (
-    <div className="flex items-center gap-2 p-2">
-      <BrainCog className="text-primary size-8" />
-      <span className="text-xl font-bold">AgentX</span>
+    <div className="flex items-center justify-between px-2 py-3">
+      <div className="flex items-center space-x-2">
+        <BrainCog className="size-7" />
+        <div className="text-xl font-bold">AgentX</div>
+      </div>
+      <div>
+        <User2Icon className="size-7" />
+      </div>
     </div>
   )
 }
