5770c3fae8b0a8122021b408cf989ca05b9b3775
rno321 <arno.burnuk@gmail.com>
2025-01-04 12:17:55 -0500

    aligned feedback btn

--- components/chat/chat-ui.tsx
+++ components/chat/chat-ui.tsx
@@ -193,12 +193,12 @@ export const ChatUI: FC<ChatUIProps> = ({}) => {
         <ChatInput />
       </div>
 
-      <div className="absolute bottom-3 right-4 flex h-[40px] items-center space-x-2 sm:bottom-8">
+      <div className="absolute bottom-4 right-4 flex h-[40px] items-center space-x-2">
         <button
-          className="flex size-10 items-center justify-center rounded-lg bg-gray-100 text-gray-600 hover:bg-gray-200 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700"
+          className="flex size-9 items-center justify-center rounded-full bg-gray-100 text-gray-600 hover:bg-gray-200 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700"
           onClick={() => setShowFeedback(true)}
         >
-          <IconMessageCircle2 size={20} />
+          <IconMessageCircle2 className="size-5" />
         </button>
       </div>
 
