bc934575a0a4cab85a290b8429894f42ae03138a
rno321 <arno.burnuk@gmail.com>
2025-01-03 20:24:16 -0500

    removed info icon

--- components/chat/chat-ui.tsx
+++ components/chat/chat-ui.tsx
@@ -12,7 +12,6 @@ import useHotkey from "@/lib/hooks/use-hotkey"
 import { LLMID, MessageImage } from "@/types"
 import { useParams } from "next/navigation"
 import { FC, useContext, useEffect, useState } from "react"
-import { ChatHelp } from "./chat-help"
 import { useScroll } from "./chat-hooks/use-scroll"
 import { ChatInput } from "./chat-input"
 import { ChatMessages } from "./chat-messages"
@@ -49,8 +48,8 @@ export const ChatUI: FC<ChatUIProps> = ({}) => {
     handleScroll,
     scrollToBottom,
     setIsAtBottom,
-    isAtTop,
     isAtBottom,
+    isAtTop,
     isOverflowing,
     scrollToTop
   } = useScroll()
@@ -58,56 +57,36 @@ export const ChatUI: FC<ChatUIProps> = ({}) => {
   const [loading, setLoading] = useState(true)
 
   useEffect(() => {
+    if (!params.chatid) return
+
     const fetchData = async () => {
-      await fetchMessages()
       await fetchChat()
-
-      scrollToBottom()
-      setIsAtBottom(true)
-    }
-
-    if (params.chatid) {
-      fetchData().then(() => {
-        handleFocusChatInput()
-        setLoading(false)
-      })
-    } else {
+      await fetchMessages()
       setLoading(false)
     }
-  }, [])
+
+    fetchData()
+  }, [params.chatid])
 
   const fetchMessages = async () => {
     const fetchedMessages = await getMessagesByChatId(params.chatid as string)
 
-    const imagePromises: Promise<MessageImage>[] = fetchedMessages.flatMap(
-      message =>
-        message.image_paths
-          ? message.image_paths.map(async imagePath => {
-              const url = await getMessageImageFromStorage(imagePath)
+    const imagePromises = await Promise.all(
+      fetchedMessages.map(async message => {
+        if (!message.image_paths) return []
 
-              if (url) {
-                const response = await fetch(url)
-                const blob = await response.blob()
-                const base64 = await convertBlobToBase64(blob)
+        return message.image_paths.map(async imagePath => {
+          const { url } = await getMessageImageFromStorage(imagePath)
 
-                return {
-                  messageId: message.id,
-                  path: imagePath,
-                  base64,
-                  url,
-                  file: null
-                }
-              }
-
-              return {
-                messageId: message.id,
-                path: imagePath,
-                base64: "",
-                url,
-                file: null
-              }
-            })
-          : []
+          return {
+            messageId: message.id,
+            path: imagePath,
+            base64: "",
+            url,
+            file: null
+          }
+        })
+      })
     )
 
     const images: MessageImage[] = await Promise.all(imagePromises.flat())
@@ -201,14 +180,8 @@ export const ChatUI: FC<ChatUIProps> = ({}) => {
         <ChatSecondaryButtons />
       </div>
 
-      <div className="bg-secondary flex max-h-[50px] min-h-[50px] w-full items-center justify-center border-b-2 font-bold">
-        <div className="max-w-[200px] truncate sm:max-w-[400px] md:max-w-[500px] lg:max-w-[600px] xl:max-w-[700px]">
-          {selectedChat?.name || "Chat"}
-        </div>
-      </div>
-
       <div
-        className="flex size-full flex-col overflow-auto border-b"
+        className="flex size-full flex-col overflow-y-auto px-4"
         onScroll={handleScroll}
       >
         <div ref={messagesStartRef} />
@@ -218,12 +191,8 @@ export const ChatUI: FC<ChatUIProps> = ({}) => {
         <div ref={messagesEndRef} />
       </div>
 
-      <div className="relative w-full min-w-[300px] items-end px-2 pb-3 pt-0 sm:w-[600px] sm:pb-8 sm:pt-5 md:w-[700px] lg:w-[700px] xl:w-[800px]">
+      <div className="w-full min-w-[300px] items-end px-2 pb-3 pt-0 sm:w-[600px] sm:pb-8 sm:pt-5 md:w-[700px] lg:w-[700px] xl:w-[800px]">
         <ChatInput />
-      </div>
-
-      <div className="absolute bottom-2 right-2 hidden md:block lg:bottom-4 lg:right-4">
-        <ChatHelp />
       </div>
     </div>
   )
